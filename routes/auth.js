578e0896a366395cbb051f8957c5ec797585dc89
Muhammad Nurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2021-10-11 11:06:25 +0700

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -63,6 +63,7 @@ router.post("/login", function (req, res) {
         return res.redirect("/");
       }
     });
+
     conn.release();
   });
 });
--- routes/content.js
+++ routes/content.js
@@ -43,7 +43,7 @@ router.post("/add-artist", upload_artist("image"), function (req, res) {
 
 // render add music page
 router.get("/add-music", function (req, res) {
-  res.render("content/add-music", { title: "Add Music", isLogin: true, css : "/static/css/auth.css" });
+  res.render("content/add-music", { title: "Add Music", isLogin: true, css : "/static/css/auth.css"});
 });
 
 router.post("/add-music", upload_music("music"), function (req, res) {
@@ -70,6 +70,114 @@ router.post("/add-music", upload_music("music"), function (req, res) {
         res.redirect("/");
       }
     });
+    conn.release();
+  });
+});
+
+// handle delete music
+router.get("/delete/:id", function (req, res) {
+  const { id } = req.params;
+
+  const query = "DELETE FROM tb_music WHERE id = ?";
+
+  dbConnection.getConnection((err, conn) => {
+    if (err) throw err;
+
+    conn.query(query, [id], (err, results) => {
+      if (err) {
+        req.session.message = {
+          type: "danger",
+          message: err.message,
+        };
+        res.redirect("/");
+      }
+
+      req.session.message = {
+        type: "success",
+        message: "music successfully deleted",
+      };
+      res.redirect("/");
+    });
+
+    conn.release();
+  });
+});
+
+// handle edit music
+router.get("/edit/:id", function (req, res) {
+  const { id } = req.params;
+
+  if (!req.session.isLogin) {
+    req.session.message = {
+      type: "danger",
+      message: "you must login first",
+    };
+
+    return res.redirect("/login");
+  }
+
+  const query = "SELECT * FROM tb_music WHERE id = ?";
+
+  dbConnection.getConnection((err, conn) => {
+    if (err) {
+      throw err;
+    }
+
+    conn.query(query, [id], (err, results) => {
+      if (err) throw err;
+
+      let music = []
+      for (let result of results) {
+        music.push({
+          ...result,
+          music: "/uploads/music/" + result.music,
+          });
+        }
+
+      req.session.message = {
+        type: "success",
+        message: "edit music successfully",
+      };
+      res.render("content/form-edit", {
+        title: "Edit Music",
+        isLogin: req.session.isLogin,
+        css : "/static/css/auth.css",
+        music
+      });
+    });
+
+    conn.release();
+  });
+});
+
+router.post("/edit/:id", upload_music("music"), function (req, res) {
+  let { id, title, music } = req.body;
+  
+  if (req.file) {
+    music = req.file.filename;
+  }
+
+  const query = "UPDATE tb_music SET title = ?, music = ? WHERE id = ?";
+
+  dbConnection.getConnection((err, conn) => {
+    if (err) throw err;
+
+    conn.query(query, [title, music, id], (err, results) => {
+      if (err) {
+        req.session.message = {
+          type: "danger",
+          message: "server error",
+        };
+        return res.redirect("/");        
+      } else {
+        req.session.message = {
+          type: "success",
+          message: "edit music successfully",
+        };
+        res.redirect("/");
+      }
+    });
+
     conn.release();
   });
 });
